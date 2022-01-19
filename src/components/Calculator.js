85724070680f5c02be4557fb13f7a8eb584c83da
NataliaPoletaeva <natfly799@gmail.com>
2022-01-19 22:02:19 +0500

    Add all the components and basic styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,31 +14,34 @@ const Calculator = () => {
 
   return (
     <main>
-      <div className="output">
-        {state.total}
-        {state.operation}
-        {state.next}
-      </div>
-      <div className="btn-wrapper">
-        <button type="button" onClick={clickEvent}>AC</button>
-        <button type="button" onClick={clickEvent}>+/-</button>
-        <button type="button" onClick={clickEvent}>%</button>
-        <button type="button" className="orange" onClick={clickEvent}>&#247;</button>
-        <button type="button" onClick={clickEvent}>7</button>
-        <button type="button" onClick={clickEvent}>8</button>
-        <button type="button" onClick={clickEvent}>9</button>
-        <button type="button" className="orange" onClick={clickEvent}>x</button>
-        <button type="button" onClick={clickEvent}>4</button>
-        <button type="button" onClick={clickEvent}>5</button>
-        <button type="button" onClick={clickEvent}>6</button>
-        <button type="button" className="orange" onClick={clickEvent}>-</button>
-        <button type="button" onClick={clickEvent}>1</button>
-        <button type="button" onClick={clickEvent}>2</button>
-        <button type="button" onClick={clickEvent}>3</button>
-        <button type="button" className="orange" onClick={clickEvent}>+</button>
-        <button type="button" className="zero" onClick={clickEvent}>0</button>
-        <button type="button" onClick={clickEvent}>.</button>
-        <button type="button" className="orange" onClick={clickEvent}>=</button>
+      <h2 className="calc-title">Let&apos;s do some math!</h2>
+      <div className="calc">
+        <div className="output">
+          {state.total}
+          {state.operation}
+          {state.next}
+        </div>
+        <div className="btn-wrapper">
+          <button type="button" onClick={clickEvent}>AC</button>
+          <button type="button" onClick={clickEvent}>+/-</button>
+          <button type="button" onClick={clickEvent}>%</button>
+          <button type="button" className="orange" onClick={clickEvent}>&#247;</button>
+          <button type="button" onClick={clickEvent}>7</button>
+          <button type="button" onClick={clickEvent}>8</button>
+          <button type="button" onClick={clickEvent}>9</button>
+          <button type="button" className="orange" onClick={clickEvent}>x</button>
+          <button type="button" onClick={clickEvent}>4</button>
+          <button type="button" onClick={clickEvent}>5</button>
+          <button type="button" onClick={clickEvent}>6</button>
+          <button type="button" className="orange" onClick={clickEvent}>-</button>
+          <button type="button" onClick={clickEvent}>1</button>
+          <button type="button" onClick={clickEvent}>2</button>
+          <button type="button" onClick={clickEvent}>3</button>
+          <button type="button" className="orange" onClick={clickEvent}>+</button>
+          <button type="button" className="zero" onClick={clickEvent}>0</button>
+          <button type="button" onClick={clickEvent}>.</button>
+          <button type="button" className="orange" onClick={clickEvent}>=</button>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Calculator.css';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to our page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua.
+      Faucibus scelerisque eleifend donec pretium
+      vulputate sapien nec sagittis aliquam.
+      Eu volutpat odio facilisis mauris sit.
+      Lorem ipsum dolor sit amet consectetur adipiscing.
+      Nisl purus in mollis nunc sed id semper risus in.
+      Volutpat maecenas volutpat blandit
+      aliquam etiam erat velit scelerisque.
+      Nunc lobortis mattis aliquam faucibus purus in.
+      Leo vel orci porta non pulvinar neque.
+      Commodo elit at imperdiet dui. Nisi vitae suscipit
+      tellus mauris. Tempus urna et pharetra pharetra massa.
+      Vitae semper quis lectus nulla at volutpat.
+      Et ligula ullamcorper malesuada proin libero nunc consequat.
+      Odio facilisis mauris sit amet massa
+      vitae tortor condimentum lacinia.
+      Nibh sed pulvinar proin gravida hendrerit lectus a.
+    </p>
+    <p>
+      Viverra justo nec ultrices dui sapien eget mi proin.
+      Hendrerit gravida rutrum quisque non tellus orci.
+      Sit amet facilisis magna etiam tempor.
+      Diam vulputate ut pharetra sit amet.
+      Molestie at elementum eu facilisis.
+      Libero id faucibus nisl tincidunt eget nullam non.
+      Amet justo donec enim diam vulputate ut pharetra.
+      Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis.
+      Magnis dis parturient montes nascetur ridiculus
+      mus mauris vitae ultricies.
+      Justo eget magna fermentum iaculis eu non diam phasellus.
+      Imperdiet sed euismod nisi porta lorem mollis aliquam.
+      Nulla pharetra diam sit amet nisl suscipit.
+      Viverra ipsum nunc aliquet bibendum. Diam in arcu cursus euismod.
+      Feugiat scelerisque varius morbi
+      enim nunc faucibus.
+      Ullamcorper velit sed ullamcorper morbi tincidunt ornare.
+      Ornare quam viverra orci sagittis eu volutpat odio.
+    </p>
+  </div>
+);
+
+export default Home;
